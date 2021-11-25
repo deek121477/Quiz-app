207814dd52c4567af2590d389e187d9bd7f63877
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2021-11-25 17:19:12 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not an open source software?",
+    answer: "Microsoft Office",
+    options: [
+      "MySQL",
+      "GNU image manipulation",
+      "Microsoft Office",
+      "Libre Office"
+    ]
+  },
+    {
+    numb: 2,
+    question: "An example of web design OSS",
+    answer: "Nvu",
+    options: [
+      "Open Office",
+      "Nvu",
+      "AbiWorld",
+      "KOffice"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What do you understand from GNOME and KDE?",
+    answer: "GUI Based Linux",
+    options: [
+      "Linux Distribution",
+      "Command Lines",
+      "GUI Based Linux",
+      " File Framework"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who was the founder of Linux?",
+    answer: "Linus Torvalds",
+    options: [
+      "Bill Gates",
+      "Christopher Markin",
+      "IBM",
+      "Linus Torvalds"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the least amount of memory you would require to run a Linux?",
+    answer: "4 Megabyte (Mb)",
+    options: [
+      "4 Megabyte (Mb)",
+      "2 Megabyte (Mb)",
+      "64 Megabyte (Mb)",
+      "16 Megabyte (Mb)"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following is a way to associate styles with your HTML document?",
+    answer: "Both of the above",
+    options: [
+      " External CSS - The Element",
+      "Imported CSS - @import Rule",
+      "Both of the above",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following property is used to set the background color of an element?",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "background-image",
+      "background-repeat",
+      "background-position"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following property specifies the distance between a marker and the text in the list?",
+    answer: "marker-offset",
+    options: [
+      "list-style-position",
+      "marker-offset",
+      "list-style-image",
+      "list-style"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following property is used to set the width of an image?",
+    answer: "width",
+    options: [
+      "border",
+      "width",
+      "-moz-opacity",
+      "height"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following property changes the style of right border?",
+    answer: ":border-right-style",
+    options: [
+      ":border-bottom-style",
+      ":border-left-style",
+      ":border-top-style",
+      ":border-right-style"
+    ]
+  },
+  
+];
